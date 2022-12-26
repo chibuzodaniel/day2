5531a75e54c1366294abd09715b99e6fd198e811
chibuzodaniel <chibuzodaniel71@gmail.com>
2022-12-26 10:08:34 +0000

    initial commit

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 var email = document.forms["form"]["username"];
 var password = document.forms["form"]["password"];
 
-var email_error = document.getElementsByClassName(".email_error");
-var password_error = document.getElementsByClassName(".password_error");
+var email_error = document.getElementById("email_error");
+var password_error = document.getElementById("password_error");
 
-function valided() {
-  if (email.value.length < 9) {
+email.addEventListener("textinput", email_verify);
+password.addEventListener("textinput", password_verify);
+
+function validated() {
+  if (email.value.length < 8) {
     email.style.border = "1px solid red";
     email_error.style.display = "block";
     email.focus();
     return false;
+  }
+  if (password.value.length < 8) {
+    password.style.border = "1px solid red";
+    password_error.style.display = "block";
+    password.focus();
+    return false;
+  }
+}
+function email_verify() {
+  if (email.value.length >= 8) {
+    email.style.border = "1px solid silver";
+    email_error.style.display = "none";
+    return true;
+  }
+}
+
+function password_verify() {
+  if (password.value.length >= 8) {
+    password.style.border = "1px solid silver";
+    password_error.style.display = "none";
+    return true;
   }
 }
